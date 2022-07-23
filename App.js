10728861ba8e1a039cdd2a6e5bbe69fca5fb2ec1
Mossalli Al Mamun <mamunorb@gmail.com>
2022-07-23 18:35:12 +0600

    redux configured

--- App.js
+++ App.js
@@ -11,14 +11,15 @@ import Router from './Router';
 import {Provider} from 'react-redux';
 import {store, persistor} from './src/redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
-import {LogBox, View, Text} from 'react-native';
-
-// LogBox.ignoreLogs([
-//   "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
-// ]);
 
 const App = () => {
-  return <Router />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router />
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default App;
--- Router.js
+++ Router.js
@@ -20,6 +20,16 @@ import {
 import HomeScreen from './src/screens/home';
 import ExpensesLog from './src/screens/expensesLog';
 
+// function Feed({navigation}) {
+//   return (
+//     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//       <Text>Feed Screen</Text>
+//       <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
+//       <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
+//     </View>
+//   );
+// }
+
 function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
