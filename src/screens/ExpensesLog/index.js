667236d605a22ce586377f0bf0e4c16a082b303c
Mossalli Al Mamun <mamunorb@gmail.com>
2022-07-25 01:12:39 +0600

    Filter by category added on expenses log

--- src/screens/ExpensesLog/index.js
+++ src/screens/ExpensesLog/index.js
@@ -1,21 +1,24 @@
 import * as React from 'react';
 import {
-  Button,
   View,
-  TextInput,
   Text,
   FlatList,
   TouchableOpacity,
+  Modal,
+  Button,
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-import {setExpensesValue} from '../../redux/reducers/expenses';
 import {Styles} from './styles';
 import dayjs from 'dayjs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 let data = [];
+var filterCategory = [];
+var uniqueFilterCategory = [];
 
 const ExpensesLog = ({navigation}) => {
+  const [filterStatus, setFilterStatus] = React.useState(false);
+  const [filterValue, setFilterValue] = React.useState('');
   const getFromRedux = useSelector(state => state.expenses);
   data = getFromRedux.value.length > 0 ? JSON.parse(getFromRedux.value) : [];
 
@@ -34,10 +37,31 @@ const ExpensesLog = ({navigation}) => {
   });
 
   let itemsWithoutZeroAmount = allData.filter(items => items.amount !== 0);
+  let itemsWithFiltered = itemsWithoutZeroAmount.filter(
+    items => items.category === filterValue,
+  );
   let totalExpensesAmount = newItems.reduce(
     (total, obj) => obj.subTotal + total,
     0,
   );
+  const openFilterModal = () => {
+    itemsWithoutZeroAmount.forEach(items => {
+      filterCategory.push(items.category);
+    });
+    uniqueFilterCategory = [...new Set(filterCategory)];
+    setFilterStatus(!filterStatus);
+  };
+
+  const filterAction = selectedValue => {
+    setFilterValue(selectedValue);
+
+    setFilterStatus(!filterStatus);
+  };
+
+  const clearFilter = () => {
+    setFilterValue('');
+    setFilterStatus(false);
+  };
 
   const renderAllExpensesLogCard = ({item}) => (
     <View style={Styles.expenseCard}>
@@ -60,18 +84,25 @@ const ExpensesLog = ({navigation}) => {
     <View style={Styles.container}>
       <View style={Styles.filterContainer}>
         <View style={Styles.filterTextContainer}>
-          <TouchableOpacity style={{paddingHorizontal: 10, paddingVertical: 5}}>
-            <Icon name="clear" size={16} color="#00000080" />
-          </TouchableOpacity>
-          <Text style={{fontSize: 16}}>Home</Text>
+          {filterValue !== '' && (
+            <TouchableOpacity
+              style={{paddingHorizontal: 10, paddingVertical: 5}}
+              onPress={() => clearFilter()}>
+              <Icon name="clear" size={16} color="#00000080" />
+            </TouchableOpacity>
+          )}
+
+          <Text style={{fontSize: 16}}>{filterValue}</Text>
         </View>
-        <TouchableOpacity>
+        <TouchableOpacity
+          style={{paddingHorizontal: 5, paddingVertical: 5}}
+          onPress={() => openFilterModal()}>
           <Icon name="filter-list" size={30} color="#00000080" />
         </TouchableOpacity>
       </View>
       <View style={{height: 20}} />
       <FlatList
-        data={itemsWithoutZeroAmount}
+        data={filterValue !== '' ? itemsWithFiltered : itemsWithoutZeroAmount}
         renderItem={renderAllExpensesLogCard}
         keyExtractor={(item, index) => index}
         showsVerticalScrollIndicator={false}
@@ -91,6 +122,40 @@ const ExpensesLog = ({navigation}) => {
         </Text>
       </View>
       <View style={{height: 30}} />
+
+      {/* // Filter Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={filterStatus}
+        hardwareAccelerated={true}
+        onRequestClose={() => {
+          setFilterStatus(!filterStatus);
+        }}>
+        <View style={Styles.modalContainer}>
+          <View style={Styles.modalItemContainer}>
+            <View style={Styles.filteredItemContainer}>
+              {uniqueFilterCategory.map((item, key) => (
+                <TouchableOpacity
+                  key={key}
+                  style={Styles.filteredItemButton}
+                  onPress={() => {
+                    filterAction(item);
+                  }}>
+                  <Text style={{fontSize: 16}}>{item}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+            <View style={Styles.modalButtonContainer}>
+              <Button
+                onPress={() => setFilterStatus(!filterStatus)}
+                title="Cancel"
+              />
+              <Button onPress={() => addCategory()} title="Filter" />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
